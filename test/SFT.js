6bc25d012b22bf57395c7822cfb1ef76a536b421
escx <1768810189@qq.com>
2024-03-09 11:23:17 +0800

    使用loadFixture代替beforeEach

--- test/SFT.js
+++ test/SFT.js
@@ -1,23 +1,32 @@
 const { expect, assert } = require("chai");
-const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"; // 零地址
+const INIT_AMOUNT = 100; // 账户初始金额
+const ALLOWANCE_AMOUNT = 200; // deployer账户授权addr1账户的额度
+
+async function initFixture() {
+  const SFTContract = await ethers.deployContract("SFT");
+  const ownerAddress = await SFTContract.owner();
+  const [deployer, addr1, addr2, _] = await ethers.getSigners();
+
+  await SFTContract.mint(INIT_AMOUNT);
+  await SFTContract.approve(addr1.address, ALLOWANCE_AMOUNT);
+
+  return { SFTContract, ownerAddress, deployer, addr1, addr2 };
+}
 
 describe("SFT Contract", function () {
-  let SFTContract, ownerAddress, deployer, addr1, addr2;
-
-  beforeEach(async function () {
-    SFTContract = await ethers.deployContract("SFT");
-    ownerAddress = await SFTContract.owner();
-    [deployer, addr1, addr2, _] = await ethers.getSigners();
-  });
-
   describe("Constructor", function () {
     it("合约拥有者应该是部署者", async function () {
-      assert.equal(ownerAddress, deployer.address)
+      const { ownerAddress, deployer } = await loadFixture(initFixture);
+
+      assert.equal(ownerAddress, deployer.address);
     });
   });
 
   describe("transfer", function () {
     it("调用者账户余额小于转账金额时失败", async function () {
+      const { SFTContract, addr1, addr2 } = await loadFixture(initFixture);
       const balanceOfCaller = await SFTContract.balanceOf(addr1.address);
       const transferAmount = balanceOfCaller + BigInt(1);
 
@@ -27,6 +36,7 @@ describe("SFT Contract", function () {
     });
 
     it("转账金额为0时失败", async function () {
+      const { SFTContract, addr1, addr2 } = await loadFixture(initFixture);
       const transferAmount = 0;
 
       await expect(SFTContract.connect(addr1).transfer(addr2.address, transferAmount))
@@ -35,6 +45,7 @@ describe("SFT Contract", function () {
     });
 
     it("调用者账户余额应该减少，接受者账户余额应该增加", async function () {
+      const { SFTContract, ownerAddress, addr2 } = await loadFixture(initFixture);
       const transferAmount = 1;
 
       await SFTContract.mint(100);
@@ -50,6 +61,7 @@ describe("SFT Contract", function () {
     });
 
     it("应该触发Transfer事件", async function () {
+      const { SFTContract, ownerAddress, addr2 } = await loadFixture(initFixture);
       const transferAmount = 1;
 
       await SFTContract.mint(100);
@@ -61,6 +73,7 @@ describe("SFT Contract", function () {
 
   describe("approve", function () {
     it("应该更新调用者账户的授权额度数据", async function () {
+      const { SFTContract, deployer, addr1 } = await loadFixture(initFixture);
       const approveAmount = 1;
       await SFTContract.approve(addr1.address, approveAmount);
 
@@ -69,6 +82,7 @@ describe("SFT Contract", function () {
     });
 
     it("应该触发Approval事件", async function () {
+      const { SFTContract, deployer, addr1 } = await loadFixture(initFixture);
       const approveAmount = 1;
 
       await expect(SFTContract.approve(addr1.address, approveAmount))
@@ -78,24 +92,18 @@ describe("SFT Contract", function () {
   });
 
   describe("transferFrom", function () {
-    const initAmount = 100;
-    const allowanceAmount = 200;
-
-    beforeEach(async function () {
-      await SFTContract.mint(initAmount);
-      await SFTContract.approve(addr1.address, allowanceAmount);
-    });
-
     it("调用者被授权额度小于转账金额时失败", async function () {
-      const transferAmount = allowanceAmount + 1;
+      const { SFTContract, deployer, addr1, addr2 } = await loadFixture(initFixture);
+      const transferAmount = ALLOWANCE_AMOUNT + 1;
 
       await expect(SFTContract.connect(addr1).transferFrom(deployer.address, addr2.address, transferAmount))
         .to.be.revertedWithCustomError(SFTContract, "ERC20InsufficientAllowance")
-        .withArgs(addr1.address, allowanceAmount, transferAmount);
+        .withArgs(addr1.address, ALLOWANCE_AMOUNT, transferAmount);
     });
 
     it("发送者账户余额小于转账金额时失败", async function () {
-      const transferAmount = initAmount + 1;
+      const { SFTContract, deployer, addr1, addr2 } = await loadFixture(initFixture);
+      const transferAmount = INIT_AMOUNT + 1;
       const balanceOfSender = await SFTContract.balanceOf(deployer.address);
 
       await expect(SFTContract.connect(addr1).transferFrom(deployer.address, addr2.address, transferAmount))
@@ -104,6 +112,7 @@ describe("SFT Contract", function () {
     });
 
     it("转账金额为0时失败", async function () {
+      const { SFTContract, deployer, addr1, addr2 } = await loadFixture(initFixture);
       const transferAmount = 0;
 
       await expect(SFTContract.connect(addr1).transferFrom(deployer.address, addr2.address, transferAmount))
@@ -112,6 +121,7 @@ describe("SFT Contract", function () {
     });
 
     it("转账成功后，发送者账户余额应该减少，接收者账户余额应该增加，同时发送者账户的授权额度更新", async function () {
+      const { SFTContract, deployer, addr1, addr2 } = await loadFixture(initFixture);
       const transferAmount = 50;
       const balanceOfSenderBefore = await SFTContract.balanceOf(deployer.address);
       const balanceOfReceiverBefore = await SFTContract.balanceOf(addr2.address);
@@ -128,6 +138,7 @@ describe("SFT Contract", function () {
     });
 
     it("应该触发Transfer事件", async function () {
+      const { SFTContract, deployer, addr1, addr2 } = await loadFixture(initFixture);
       const transferAmount = 50;
 
       await expect(SFTContract.connect(addr1).transferFrom(deployer.address, addr2.address, transferAmount))
@@ -138,6 +149,7 @@ describe("SFT Contract", function () {
 
   describe("mint", function () {
     it("合约拥有者账户余额应该增加，总供应量应该增加", async function () {
+      const { SFTContract, ownerAddress } = await loadFixture(initFixture);
       const mintAmount = 100;
       const balanceOfOwnerBefore = await SFTContract.balanceOf(ownerAddress);
       const supplyBefore = await SFTContract.totalSupply();
@@ -151,6 +163,7 @@ describe("SFT Contract", function () {
     });
 
     it("应该触发Transfer事件", async function () {
+      const { SFTContract, ownerAddress } = await loadFixture(initFixture);
       const mintAmount = 100;
 
       await expect(SFTContract.mint(mintAmount))
@@ -160,15 +173,10 @@ describe("SFT Contract", function () {
   });
 
   describe("burn", function () {
-    const initAmount = 100;
-
-    beforeEach(async function () {
-      await SFTContract.mint(initAmount);
-    });
-
     it("合约拥有者账户余额小于销毁数量时失败", async function () {
+      const { SFTContract, ownerAddress } = await loadFixture(initFixture);
       const balanceOfOwner = await SFTContract.balanceOf(ownerAddress);
-      const burnAmount = initAmount + 1;
+      const burnAmount = INIT_AMOUNT + 1;
 
       await expect(SFTContract.burn(burnAmount))
         .to.be.revertedWithCustomError(SFTContract, "ERC20InsufficientBalance")
@@ -176,6 +184,7 @@ describe("SFT Contract", function () {
     });
 
     it("合约拥有者账户余额应该减少，总供应量应该减少", async function () {
+      const { SFTContract, ownerAddress } = await loadFixture(initFixture);
       const burnAmount = 1;
       const balanceOfOwnerBefore = await SFTContract.balanceOf(ownerAddress);
       const supplyBefore = await SFTContract.totalSupply();
@@ -189,6 +198,7 @@ describe("SFT Contract", function () {
     });
 
     it("应该触发Transfer事件", async function () {
+      const { SFTContract, ownerAddress } = await loadFixture(initFixture);
       const burnAmount = 1;
 
       await expect(SFTContract.burn(burnAmount))
