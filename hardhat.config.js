515b2a720398183250247570035bfd3fff7f7773
escx <1768810189@qq.com>
2024-03-09 13:53:53 +0800

    增加gas reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ethers");
 require("@nomicfoundation/hardhat-chai-matchers");
+require("hardhat-gas-reporter");
+require("dotenv").config();
+
+const { REPORT_GAS } = process.env;
 
 module.exports = {
   solidity: "0.8.24",
+  gasReporter: {
+    enabled: REPORT_GAS === "1" ? true : false
+  }
 };
