7844630736f963e27b0ba0c2b0ea8a2b7f0e5535
xujian <xujian@dip-ai.com>
2024-03-11 16:15:15 +0800

    部署时不再调用mint方法，计划在前台进行函数操作

--- ignition/modules/sft_local.js
+++ ignition/modules/sft_local.js
@@ -1,10 +1,7 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("sft_local", (m) => {
-  const contract = m.contract("SFT");
-  const mintValue = m.getParameter("mint_value", 1_000_000_000n);
+  const sftContract = m.contract("SFT");
 
-  m.call(contract, "mint", [mintValue]);
-
-  return { contract };
+  return { sftContract };
 });
