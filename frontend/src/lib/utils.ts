2b013fe6e6d8c813f7b65f783e2cb7c1def05d57
xujian <xujian@dip-ai.com>
2024-03-12 17:15:49 +0800

    重置前端项目，ui组件库换为ant-design

--- frontend/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export const HARDHAT_CHAINID = 31337
-export const HARDHAT_NODE_URL = "http://localhost:8545"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,14 @@
-import Head from "next/head"
 import type { AppProps } from "next/app"
-import "@/styles/globals.css"
+import Head from "next/head"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>SimFi</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <>
+    <Head>
+      <title>SimFi</title>
+      <meta name="description" content="Web for SimFi" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+
+    <Component {...pageProps} />
+  </>
 }
